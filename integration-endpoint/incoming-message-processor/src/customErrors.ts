e11a671de3be6ae6e70fd57145616f2c3b02e7c4
Sergei Galushin <sgalushin@et904.com>
2021-08-19 16:19:36 +0300

    Refactor incoming message error handling: APIGatewayProxyResult is created automatically in ErrorWithPredefinedResponse's constructor

--- integration-endpoint/incoming-message-processor/src/customErrors.ts
+++ integration-endpoint/incoming-message-processor/src/customErrors.ts
@@ -1,37 +1,36 @@
 import { APIGatewayProxyResult } from "aws-lambda";
 
+/**
+ * Represents an error that occurred while decoding the received message.
+ * Extends standard Error class, but also provides a variable 'response' that is a
+ * prefabricated response for AWS Lambda (including status code and response body).
+ */
 export abstract class ErrorWithPredefinedResponse extends Error {
-  abstract response: APIGatewayProxyResult;
+  public response: APIGatewayProxyResult;
+
+  constructor(errorDescription: string, statusCode = 400) {
+    super(errorDescription);
+    this.response = {
+      statusCode: statusCode,
+      body: errorDescription
+    }
+  }
 }
 
 export class ErrorBodyIsNotAJSON extends ErrorWithPredefinedResponse {
-  response: APIGatewayProxyResult = {
-    statusCode: 400,
-    body: "Body is not a valid JSON",
-  };
-}
-
-export class ErrorBodyIsNotAValidMessage extends ErrorWithPredefinedResponse {
-  response: APIGatewayProxyResult = {
-    statusCode: 400,
-    body: "Body is not a valid message: must have 'eventType' and 'payload'",
-  };
+  constructor() {
+    super("Body is not a valid JSON");
+  }
 }
 
 export class ErrorUnknownEventType extends ErrorWithPredefinedResponse {
-  response: APIGatewayProxyResult = {
-    statusCode: 400,
-    body: "Unknown event type",
-  };
+  constructor() {
+    super("Unknown event type");
+  }
 }
 
 export class ErrorBodyWasNotVerified extends ErrorWithPredefinedResponse {
   constructor(errors: string[]) {
-    super();
-    this.response.body = JSON.stringify(errors);
+    super(JSON.stringify(errors));
   }
-  response: APIGatewayProxyResult = {
-    statusCode: 400,
-    body: "",
-  };
 }
--- integration-endpoint/incoming-message-processor/src/lambda-handlers/receive-incoming-message.ts
+++ integration-endpoint/incoming-message-processor/src/lambda-handlers/receive-incoming-message.ts
@@ -1,9 +1,14 @@
 import lambda, { APIGatewayProxyResult } from "aws-lambda";
-import { validateMessage } from "./validateMessage";
-import sendMessageToQueue from "./sendToQueue";
-import { ErrorWithPredefinedResponse } from "./customErrors";
-import eventSettings from "./eventSettings";
+import { validateMessage } from "../validateMessage";
+import sendMessageToQueue from "../sendToQueue";
+import { ErrorWithPredefinedResponse } from "../customErrors";
+import eventSettings from "../eventSettings";
 
+/**
+ * A lambda handler for receiving any type of message.
+ * If a message is successfully received, returns an empty 202 responce.
+ * If while processing a message a known type of error was encountered, returns
+ */
 export const lambdaHandler = async (event: lambda.APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   try {
     const { eventType, payload } = validateMessage(event.body);
